31ba4c03b2e12c2fed8194c9e20c24db4dc1d61f
SysForgeLabs <sysforgelabs@gmail.com>
2024-09-24 15:42:00 +0000

    sll

---
+++ frontend/components/ui/input.tsx
@@ -0,0 +1,19 @@
+// ./components/ui/input.tsx
+
+import React from 'react'
+
+const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <input
+      {...props}
+      className={`px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+        className || ''
+      }`}
+    />
+  )
+}
+
+export { Input }
